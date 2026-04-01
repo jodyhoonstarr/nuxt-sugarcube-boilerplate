6f3f40eeb65c4cc5e1e967cf71fd94afd1ffbdbe
Jody Hoon-Starr <jody@protonmail.com>
2026-04-01 16:21:42 -0400

    chore: cleanup index

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,35 +4,15 @@ const routes = router.getRoutes()
 </script>
 
 <template>
-  <div>
-    <h1 class="text-black border-primary bg-secondary">
-      Nuxt + SugarCube
-    </h1>
-    <h2 class="bg-secondary">
-      Pages:
-    </h2>
-    <ol class="bg-black text-white">
-      <li>
-        <nuxt-link :to="'/'">
-          Home
-        </nuxt-link>
-      </li>
+  <article class="wrapper flow">
+    <h1>Nuxt & SugarCube</h1>
+    <h2>Examples:</h2>
+    <ul>
       <li v-for="route in routes.filter((r) => r.path !== '/')" :key="route.path">
         <nuxt-link :to="route.path">
           {{ route.path.substring(1) }}
         </nuxt-link>
       </li>
-    </ol>
-    <div id="thing" class="border-primary bg-secondary">
-      I'm a thing
-    </div>
-  </div>
+    </ul>
+  </article>
 </template>
-
-<style lang="css">
-/* make the thing have a border witdth */
-#thing {
-  border-width: 2px;
-  border-style: solid;
-}
-</style>
